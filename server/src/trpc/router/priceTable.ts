b69004bad1cf67b536eb4baa58222f6f29d094f5
Arnaud DE SAINT JEAN <arnaud.dsj@gmail.com>
2024-08-08 15:54:03 +0200

    Add a set of PriceTable Routes

---
+++ server/src/trpc/router/priceTable.ts
@@ -0,0 +1,94 @@
+import { router, protectedProcedure } from '../index'
+import { z } from 'zod'
+import { TRPCError } from '@trpc/server'
+import { PriceTable } from '../../entity/PriceTable'
+import { AppDataSource } from '../../data-source'
+
+const priceTableSchema = z.object({
+  name: z.string(),
+  generalSettings: z.object({
+    baseCurrency: z.string(),
+    availableCurrencies: z.array(z.string()),
+    generalStyle: z.string(),
+    templateId: z.string().optional(),
+    iconStyle: z.enum(['icon', 'text']),
+    paymentType: z.enum(['cycles', 'one-time', 'usage-based']),
+    cycleOptions: z.array(z.string()).optional(),
+    usageRanges: z.array(z.object({
+      min: z.number(),
+      max: z.number(),
+      price: z.number()
+    })).optional()
+  }),
+  stripePublicKey: z.string(),
+  paddlePublicKey: z.string(),
+  localizationSettings: z.object({
+    enableAutomaticCurrencyConversion: z.boolean(),
+    countrySpecificPricing: z.record(z.string())
+  })
+})
+
+export const priceTableRouter = router({
+  create: protectedProcedure
+    .input(priceTableSchema)
+    .mutation(async ({ input, ctx }) => {
+      const priceTableRepository = AppDataSource.getRepository(PriceTable)
+      const newPriceTable = priceTableRepository.create({
+        ...input,
+        user: ctx.user
+      })
+      await priceTableRepository.save(newPriceTable)
+      return newPriceTable
+    }),
+
+  getAll: protectedProcedure
+    .query(async ({ ctx }) => {
+      const priceTableRepository = AppDataSource.getRepository(PriceTable)
+      return await priceTableRepository.find({ where: { user: { id: ctx.user.id } } })
+    }),
+
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string().uuid() }))
+    .query(async ({ input, ctx }) => {
+      const priceTableRepository = AppDataSource.getRepository(PriceTable)
+      const priceTable = await priceTableRepository.findOne({
+        where: { id: input.id, user: { id: ctx.user.id } }
+      })
+      if (!priceTable) {
+        throw new TRPCError({ code: 'NOT_FOUND', message: 'Price table not found' })
+      }
+      return priceTable
+    }),
+
+  update: protectedProcedure
+    .input(z.object({
+      id: z.string().uuid(),
+      data: priceTableSchema.partial()
+    }))
+    .mutation(async ({ input, ctx }) => {
+      const priceTableRepository = AppDataSource.getRepository(PriceTable)
+      const priceTable = await priceTableRepository.findOne({
+        where: { id: input.id, user: { id: ctx.user.id } }
+      })
+      if (!priceTable) {
+        throw new TRPCError({ code: 'NOT_FOUND', message: 'Price table not found' })
+      }
+      priceTableRepository.merge(priceTable, input.data)
+      await priceTableRepository.save(priceTable)
+      return priceTable
+    }),
+
+  delete: protectedProcedure
+    .input(z.object({ id: z.string().uuid() }))
+    .mutation(async ({ input, ctx }) => {
+      const priceTableRepository = AppDataSource.getRepository(PriceTable)
+      const priceTable = await priceTableRepository.findOne({
+        where: { id: input.id, user: { id: ctx.user.id } }
+      })
+      if (!priceTable) {
+        throw new TRPCError({ code: 'NOT_FOUND', message: 'Price table not found' })
+      }
+      await priceTableRepository.remove(priceTable)
+      return { success: true }
+    })
+})
